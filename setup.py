9a01f6f8cb1eb737a47c3ea73d7e4fa9d82dad65
George Zhang <geetransit@gmail.com>
2021-07-29 19:42:48 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="soundit",
+    version="0.1.0",
+    license="MIT",
+    author="George Zhang",
+    author_email="geetransit@gmail.com",
+    description="Make audio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GeeTransit/soundit",
+    py_modules=["soundit"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Multimedia :: Sound/Audio",
+    ],
+)
